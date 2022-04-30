f35ced66be9f92bb356560258d9540b1e4c51db4
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-30 13:53:11 +0300

    created both news and Article reviews

--- app/models/article_models.py
+++ app/models/article_models.py
@@ -6,7 +6,7 @@ class Article:
         self.title = title
         self.description = description
         self.url = urlToImage
-        self.urlToImage = "https://image.tmdb.org/t/p/w500/" + poster
+        self.urlToImage = "https://image.tmdb.org/t/p/w500/" 
         self.publishedAt = publishedAt
 
 
@@ -15,28 +15,27 @@ class ArticleReview:
 
     all_reviews = []
 
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
+    def __init__(self,title,imageurl,review):
         self.title = title
         self.imageurl = imageurl
         self.review = review
 
 
     def save_review(self):
-        Review.all_reviews.append(self)
+        ArticleReview.articles_reviews.append(self)
 
 
     @classmethod
     def clear_reviews(cls):
-        Review.all_reviews.clear()
+        ArticleReview.articles_reviews.clear()
 
     @classmethod
-    def get_reviews(cls,id):
+    def get_reviews(cls,title):
 
-        response = []
+        article_response = []
 
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
+        for review in cls.articles_reviews:
+            if review.title == title:
+                article_response.append(review)
 
-        return response
+        return article_response
